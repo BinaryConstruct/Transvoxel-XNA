ba5b0537f6dacbbf34a4980a7a22836f0906c301
oggs91 <oggs@gmx.at>
2012-01-23 00:21:10 +0100

    Basic Classes for storage of voxels

---
+++ VolumeData/VolumeChunk.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VoxelStuff.helper;
+
+namespace VoxelStuff.VolumeData
+{
+    public class VolumeChunk : IVolumeData
+    {
+        public const int CHUNKSIZE = 16;
+        private readonly sbyte[] values = new sbyte[CHUNKSIZE * CHUNKSIZE * CHUNKSIZE];
+
+        public override sbyte this[int x, int y, int z] 
+        {
+            get
+            {
+                return values[Coord2Index(x,y,z)];
+            }
+
+            set
+            {
+                values[Coord2Index(x, y, z)] = value;
+            }
+        }
+
+        private int Coord2Index(int x, int y, int z)
+        {
+            return x + y * CHUNKSIZE + z * CHUNKSIZE * CHUNKSIZE;
+        }
+    }
+}
---
+++ VolumeData/VolumeData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace VoxelStuff.VolumeData
+{
+    public class VolumeData : IVolumeData
+    {
+        VolumeChunk[,,] data;
+
+        public override sbyte this[int x, int y, int z]
+        {
+            get
+            {
+                return data[x / VolumeChunk.CHUNKSIZE, y / VolumeChunk.CHUNKSIZE, z / VolumeChunk.CHUNKSIZE][x, y, z];
+            }
+
+            set
+            {
+                data[x / VolumeChunk.CHUNKSIZE, y / VolumeChunk.CHUNKSIZE, z / VolumeChunk.CHUNKSIZE][x, y, z] = value;
+            }
+        }
+    }
+}
