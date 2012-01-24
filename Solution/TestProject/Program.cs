1f7d17eca695ae26a9ead6d1cd556224ddcaf349
oggs91 <oggs@gmx.at>
2012-01-24 21:12:41 +0100

    For some tests of the octree

---
+++ Solution/TestProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TransvoxelXna.Helper;
+using TransvoxelXna.VolumeData;
+
+namespace TestProject
+{
+    // Just for some unit tests
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VolumeDataBaseOctree octree = new VolumeDataBaseOctree();
+
+            octree[0, 0, 0] = 10;
+            Console.WriteLine(octree[0, 0, 0]);
+
+
+            Console.ReadLine();
+        }
+    }
+}
