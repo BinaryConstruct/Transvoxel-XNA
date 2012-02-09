e97c2926353a359988c8380914699f3c07e8cdc1
oggs91 <oggs@gmx.at>
2012-02-09 11:23:46 +0100

    test without unittest, didn't work as expected

---
+++ Transvoxel/Test.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TransvoxelXna.VolumeData;
+using System.Diagnostics;
+using TransvoxelXna.VolumeData.CompactOctree;
+
+namespace TransvoxelXna
+{
+    public class Test
+    {
+        static void Main(string[] args)
+        {
+            IVolumeData octree = new CompactOctree();
+            Stopwatch watch = new Stopwatch();
+            int fail = 0;
+            watch.Start();
+            for (int i = -200; i < 200; i+=20)
+            {
+                for (int j = -200; j < 200; j += 50)
+                {
+                    for (int k = -200; k < 200; k += 50)
+                    {
+
+                        for (int x = -200; x < 200; x += 50)
+                        {
+                            for (int y = -200; y < 200; y += 50)
+                            {
+                                for (int z = -200; z < 200; z += 50)
+                                {
+                                    octree[x + i * 16, y + j * 16, z + k * 16] = (sbyte)(x + y + z);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            watch.Stop();
+            Console.WriteLine("Write: " + watch.ElapsedMilliseconds);
+
+            watch.Restart();
+            for (int i = -200; i < 200; i += 50)
+            {
+                for (int j = -200; j < 200; j += 50)
+                {
+                    for (int k = -200; k < 200; k += 50)
+                    {
+
+                        for (int x = -200; x < 200; x += 50)
+                        {
+                            for (int y = -200; y < 200; y += 50)
+                            {
+                                for (int z = -200; z < 200; z += 50)
+                                {
+                                    var test = octree[x + i * 16, y + j * 16, z + k * 16];
+                                    if (test != (sbyte)(x + y + z))
+                                        fail++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            watch.Stop();
+            Console.WriteLine("Read: " + watch.ElapsedMilliseconds);
+            Console.WriteLine(octree.ToString());
+            Console.WriteLine(fail);
+            Console.ReadLine();
+        }
+    }
+}
