f47dae2ab81f20c1c7bc3f05e3410796d5f497e8
BinaryConstruct <binaryconstruct@gmail.com>
2012-02-10 09:10:51 -0600

    Adding license text to Lengyl Tables

--- Projects/Transvoxel/Lengyel/Tables.cs
+++ Projects/Transvoxel/Lengyel/Tables.cs
@@ -1,3 +1,30 @@
+//================================================================================
+//
+// The Transvoxel Algorithm look-up tables
+//
+// The following data originates from Eric Lengyel's Transvoxel Algorithm.
+// http://www.terathon.com/voxels/
+//
+// The data in this file may be freely used in implementations of the Transvoxel
+// Algorithm. If you do use this data, or any transformation of it, in your own
+// projects, commercial or otherwise, please give credit by indicating in your
+// source code that the data is part of the author's implementation of the
+// Transvoxel Algorithm and that it came from the web address given above.
+// (Simply copying and pasting the two lines of the previous paragraph would be
+// perfect.) If you distribute a commercial product with source code included,
+// then the credit in the source code is required.
+//
+// If you distribute any kind of product that uses this data, a credit visible to
+// the end-user would be appreciated, but it is not required. However, you may
+// not claim that the entire implementation of the Transvoxel Algorithm is your
+// own if you use the data in this file or any transformation of it.
+//
+// The format of the data in this file is described in the dissertation "Voxel-
+// Based Terrain for Real-Time Virtual Simulations", available at the web page
+// given above. References to sections and figures below pertain to that paper.
+//
+//================================================================================
+
 using System;
 using Transvoxel.Math;
 
